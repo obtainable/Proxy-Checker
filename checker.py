11851a99f9956443ed41b6519abf8494dfc567aa
obtainable <71468624+obtainable@users.noreply.github.com>
2021-02-07 22:58:39 +0000

    Update checker.py

--- checker.py
+++ checker.py
@@ -4,8 +4,6 @@ import ctypes, time, os
 
 class Checker:
     def __init__(self):
-        self.session = requests.Session()
-        self.session.trust_env = False
         self.proxies = []
         self.counter = 0
         self.good = 0
@@ -13,17 +11,22 @@ class Checker:
 
     def save(self, proxy):
         try:
-            with open('Good.txt', 'a') as f:
-                f.write('{}\n'.format(proxy))
+            with open("Good.txt", "a") as f:
+                f.write("{}\n".format(proxy))
         except:
             pass
+       
+    def session(self):
+        session = requests.Session()
+        session.trust_env = False
+        return session
 
     def title(self):
-        ctypes.windll.kernel32.SetConsoleTitleW('Proxy Checker | Good: {0} | Bad: {1} | Checked: {2} | Remaining: {3}'.format(self.good, self.bad, (self.good + self.bad), (len(self.proxies) - (self.good + self.bad))))
+        ctypes.windll.kernel32.SetConsoleTitleW("Proxy Checker | Good: {0} | Bad: {1} | Checked: {2} | Remaining: {3}".format(self.good, self.bad, (self.good + self.bad), (len(self.proxies) - (self.good + self.bad))))
 
-    def Check(self, proxy):
+    def check_proxy(self, proxy):
         try:
-            self.session.get(self.url, proxies = {'https': 'https://{}'.format(proxy)})
+            self.session().get(self.url, proxies = {"https": "https://{}".format(proxy)})
             self.good += 1
             self.save(proxy)
         except:
@@ -31,26 +34,26 @@ class Checker:
         self.title()
 
     def proxy(self):
-        if os.path.exists('proxies.txt'):
-            with open('proxies.txt', 'r') as f:
+        if os.path.exists("proxies.txt"):
+            with open("proxies.txt", "r") as f:
                 for proxy in f:
-                    proxy = proxy.strip('\n')
+                    proxy = proxy.strip("\n")
                     self.proxies.append(proxy)
             return
-        os.system('cls'); print('\n{0} > {1}{2}Error: No proxy file found'.format(Fore.RED, Fore.WHITE, Style.BRIGHT)); time.sleep(5); exit()
+        os.system("cls"); print("\n{0} > {1}{2}Error: No proxy file found".format(Fore.RED, Fore.WHITE, Style.BRIGHT)); time.sleep(5); exit()
     
     def main(self):
-        os.system('cls'); ctypes.windll.kernel32.SetConsoleTitleW('Proxy Checker')
+        os.system("cls"); ctypes.windll.kernel32.SetConsoleTitleW("Proxy Checker")
         self.proxy()
-        self.url = str(input('\n{0} > {1}{2}URL: '.format(Fore.GREEN, Fore.WHITE, Style.BRIGHT)))
-        self.threads = int(input('{0} > {1}{2}Threads: '.format(Fore.GREEN, Fore.WHITE, Style.BRIGHT)))
+        self.url = str(input("\n{0} > {1}{2}URL: ".format(Fore.GREEN, Fore.WHITE, Style.BRIGHT)))
+        self.threads = int(input("{0} > {1}{2}Threads: ".format(Fore.GREEN, Fore.WHITE, Style.BRIGHT)))
 
-        def my_function():
-            self.Check(self.proxies[self.counter])
+        def thread_starter():
+            self.check_proxy(self.proxies[self.counter])
         
         while True:
             if threading.active_count() <= self.threads:
-                threading.Thread(target = my_function).start()
+                threading.Thread(target = thread_starter).start()
                 self.counter += 1
             if self.counter >= len(self.proxies): break
 
